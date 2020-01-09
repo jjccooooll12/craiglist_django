635f3623df67bd860eccf0c8f0fc6c61021da6e8
jcool12 <jjccooooll12@gmail.com>
2020-01-09 23:51:50 +0100

    add front-end

--- craiglist/views.py
+++ craiglist/views.py
@@ -1,4 +1,8 @@
+import requests
+from bs4 import BeautifulSoup
 from django.shortcuts import render
+from requests.compat import quote_plus
+from . import models
 
 # Create your views here.
 
@@ -6,3 +10,9 @@ context = { }
 
 def home(request):
     return render (request, 'craiglist/base.html', context)
+
+
+def new_search(request):
+    search = request.POST.get('search')
+    context = {'search':search}
+    return render(request, 'craiglist/new_search.html', context)
