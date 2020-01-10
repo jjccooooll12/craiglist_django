d0eafc9f059ffd22473de81a8536ddec9ec8132c
jcool12 <jjccooooll12@gmail.com>
2020-01-10 17:24:36 +0100

    added new_search functionalities

--- craiglist/models.py
+++ craiglist/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Search(models.Model):
-    search = models.CharField(max_length=500)
-    created = models.DateTimeField()
+    search = models.CharField(max_length=500, null=True)
+    created = models.DateTimeField(null=True)
 
-    def __str__(self): 
+    def __str__(self):
         return '{}'.format(self.search)
+
+class Meta:
+    verbose_name_plural = 'Searches'
